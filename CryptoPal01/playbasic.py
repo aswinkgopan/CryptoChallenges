d38024445d2037217b223a8138d8ef9a15599088
aswinkgopan <aswinkgopan@gmail.com>
2018-07-23 22:34:48 +0200

    Made Changes

--- CryptoPal01/playbasic.py
+++ CryptoPal01/playbasic.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-=======
-snd of time
-<<<<<<< HEAD
-snd of time
-=======
-snd of time
->>>>>>> ac8753f237a0dbfa4e6c9cd42a7eefe89e316420
->>>>>>> 0d35a3fae82cb4f6db05e9cb744e5868d1f15b55
+import requests
+import base64
+
+
+print "hello"
